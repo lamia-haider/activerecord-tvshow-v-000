2a81d95bf357e0c3bde4e46bdd165f8f5b4f9334
Lamia Haider <lamia.haider@gmail.com>
2020-09-09 20:21:34 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,4 +4,8 @@ class Show < ActiveRecord::Base
     Show.maximum(:rating)
   end
 
+  def self.most_popular_show
+    Show.where('rating = ?', self.highest_rating).first #there could be multiple shows with the same highest rating
+  end
+
 end
