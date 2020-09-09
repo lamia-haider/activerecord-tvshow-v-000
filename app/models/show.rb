125d508b92ca59c7e04b3c75b3ceadbd9d1c7789
Lamia Haider <lamia.haider@gmail.com>
2020-09-09 20:24:01 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -16,4 +16,8 @@ class Show < ActiveRecord::Base
     Show.where('rating = ?', self.lowest_rating).first #there could be multiple shows with the same highest rating
   end
 
+  def ratings_sum
+    Show.sum("rating")
+  end
+
 end
